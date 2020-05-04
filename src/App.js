4d9d857979b1d3379c4a936136218b0a151ce11d
Vivek Mittal <42748450+vivek1909@users.noreply.github.com>
2020-05-04 23:51:13 +0530

    class to functional

--- src/App.js
+++ src/App.js
@@ -1,41 +1,33 @@
-import React, {Component} from 'react';
-import Navigation from './components/Navigation/Navigation';
-import Logo from './components/Logo/Logo';
-import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
-import './App.css';
-import Particles from 'react-particles-js';
+import React, { Component } from "react";
+import Navigation from "./components/Navigation/Navigation";
+import Logo from "./components/Logo/Logo";
+import ImageLinkForm from "./components/ImageLinkForm/ImageLinkForm";
+import "./App.css";
+import Particles from "react-particles-js";
 
+const particlesOptions = {
+  particles: {
+    number: {
+      value: 100,
+      density: {
+        enable: true,
+        value_area: 300,
+      },
+    },
+  },
+};
 
-const particlesOptions ={
-  
-    particles: {
-      number: {
-        value :100,
-        density: {
-          enable:true,
-          value_area:300
-        }
-      }
-      
-      }
-    }
-  
-
-class App extends Component {
-  render() {
+// don't use class components as they are not used anymore, instead use REACT HOOKS with functional components
+const App = () => {
   return (
     <div className="App">
-       <Particles className='particles'
-              params={particlesOptions}
-              
-            />
+      <Particles className="particles" params={particlesOptions} />
       <Navigation />
       <Logo />
       <ImageLinkForm />
       {/* <FaceRecognition /> */}
     </div>
   );
-  }
-}
+};
 
 export default App;
